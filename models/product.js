87097a31775ca7387ff4e245eb962f398325b436
leedinh <ltddd3006@gmail.com>
2021-11-21 22:38:33 +0700

    [c] add pic

--- models/product.js
+++ models/product.js
@@ -2,9 +2,9 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var schema = new Schema({
-    imagePath: { type: String, required: true },
-    title: { type: String, required: true },
-    description: { type: String, required: true },
+    imagepath: { type: String, required: true },
+    name: { type: String, required: true },
+    description: { type: String, required: false },
     price: { type: Number, required: true },
     category: { type: String, required: true }
 });
